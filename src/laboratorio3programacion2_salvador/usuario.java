7cc8a1d2f0a0d41237ecd589f16aa9662c46a381
papilon10 <140213599+papilon10@users.noreply.github.com>
2024-08-02 13:34:40 -0600

    clase usuario

--- src/laboratorio3programacion2_salvador/usuario.java
+++ src/laboratorio3programacion2_salvador/usuario.java
@@ -9,5 +9,64 @@ package laboratorio3programacion2_salvador;
  * @author Apple
  */
 public class usuario {
-    
+
+    private String nombre;
+    private String usuario;
+    private String contrasena;
+
+    private enum universidad {
+        UNAH("UNAH"),
+        UNITEC("UNITEC"),
+        UNICAH("UNICAH"),
+        UJCV("UJCV"),
+        UTH("UTH"),
+        UMH("UMH");
+
+        private String nombre_uni;
+
+        private universidad(String nombre_uni) {
+            this.nombre_uni = nombre_uni;
+        }
+
+        public String getNombre_uni() {
+            return nombre_uni;
+        }
+
+    }
+
+    public usuario(String nombre, String usuario, String contrasena) {
+        this.nombre = nombre;
+        this.usuario = usuario;
+        this.contrasena = contrasena;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    @Override
+    public String toString() {
+        return "usuario{" + "nombre=" + nombre + ", usuario=" + usuario + ", contrasena=" + contrasena + '}';
+    }
+
 }
