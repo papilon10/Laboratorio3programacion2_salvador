e34679da9a36aeb717bc9fcac6e22ea1431a6b25
papilon10 <140213599+papilon10@users.noreply.github.com>
2024-08-02 13:25:52 -0600

    inicio/primer commit

---
+++ src/laboratorio3programacion2_salvador/Laboratorio3programacion2_salvador.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package laboratorio3programacion2_salvador;
+
+/**
+ *
+ * @author Apple fila2 asiento2
+ */
+public class Laboratorio3programacion2_salvador {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+        
+    }
+    
+}
